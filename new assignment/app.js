b4cbbf7510373cd17e91c48e9e36cbc81860cdd3
SYED MUSAB <92291860+SYEDMUSAB125@users.noreply.github.com>
2026-05-17 17:06:23 +0500

    Add overlay functionality for sidebar navigation and enhance styling

--- new assignment/app.js
+++ new assignment/app.js
@@ -1,23 +1,31 @@
 const toggleBtn = document.getElementById("toggleBtn");
-
 const container = document.getElementById("container");
-
 const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
 
 toggleBtn.addEventListener("click",()=>{
 
-  // Desktop Sidebar Collapse
+  // Desktop
   if(window.innerWidth > 768){
 
     container.classList.toggle("active");
 
   }
 
-  // Mobile Sidebar Open
+  // Mobile
   else{
 
     sidebar.classList.toggle("show");
+    overlay.classList.toggle("show");
 
   }
+
+});
+
+// Close sidebar when clicking overlay
+overlay.addEventListener("click",()=>{
+
+  sidebar.classList.remove("show");
+  overlay.classList.remove("show");
 
 });
